a3bad2c8fcf7bff9c7a649b409e776e346c4fa39
Julien LaPointe <email@julienlapointe.com>
2017-07-31 16:54:45 -0500

    used the NodeJS 'events' module to create a Person object that inherits the EventEmitter .on() and .emit() methods via the NodeJS 'util' module and its util.inherits() method

--- 3-eventEmitter.js
+++ 3-eventEmitter.js
@@ -1,13 +1,14 @@
 // add the NodeJS "events" modules
-var events = require("events");
+const events = require("events");
 
 // EventEmitter is a constructor
 // create a new instance of EventEmitter
 // emitter now has 2 methods:
 	// 1. emitter.on()
 	// 2. emitter.emit()
-var emitter = new events.EventEmitter();
+const emitter = new events.EventEmitter();
 
+// NOTE: the .on() listen method must be placed *BEFORE* the .emit() emit method (otherwise, the event will be emitted, but nothing will be listening for it...)
 // listens for "customEvent"
 // when "customEvent" occurs, the "status" and "message" variables are delivered into the callback function's parameters 
 // param #1: custom event name to listen for
---
+++ 4-eventEmitter.js
@@ -0,0 +1,30 @@
+// add the NodeJS "events" modules
+// pull EventEmitter constructor function out of the NodeJS "events" module and assign it to a local variable
+// var "EventEmitter" is a constructor function
+const EventEmitter = require("events").EventEmitter;
+// add the NodeJS "utilities" modules
+const util = require("util");
+
+// create a constructor function for a Person
+// NOTE: cannot use arrow functions (=>) as constructor functions
+// const Person = (name) => {
+const Person = function(name) {
+	// set *this* person's name to the "name" paramter passed in
+	this.name = name;
+}
+
+// Person object to inherit the EventEmitter constructor function / object (because everything in JS in an object)
+// .inherits() method allows us to add an object to the prototype of an existing object 
+// this means that the Person object has inheritted all the functonality of the EventEmitter object
+util.inherits(Person, EventEmitter);
+
+var steveJobs = new Person("Steve Jobs");
+
+// console.log(steveJobs.name);
+
+steveJobs.on("speak", function(said) {
+	// "this" = steveJobs
+	console.log(`${this.name}: ${said}`);
+});
+
+steveJobs.emit("speak", "Keep it simple stupid.");
